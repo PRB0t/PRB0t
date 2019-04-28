71a2b8873a91cdd2a33a96d9c9b8a2eaeb6b57e9
ideabile <info@ideabile.com>
2019-04-28 11:33:12 +0200

    Add CORS option for now.sh implementation. - 📦 add micro-cors - ⚙️  define ENV CORS

--- src/handlers/micro.js
+++ src/handlers/micro.js
@@ -1,19 +1,26 @@
 const PR = require('./../PullRequest').default;
 const {json} = require('micro');
+const origin = process.env.HOST_NAME || null;
+const microCors = require('micro-cors');
 
 const handler = async (req, res) => {
+
     try {
-        const body = await json(req)
+
+        const body = await json(req);
         const pr = new PR(body.user, body.repo, body.branch, body.token);
         pr.configure(body.files, body.commit, body.title, body.description);
         const { data } = await pr.send();
         return data;
+
     } catch(err) {
+
         console.log(err);
-        const error = new Error(`Pull request can't be created!`)
-        error.statusCode = 500
-        throw error
+        const error = new Error(`Pull request can't be created!`);
+        error.statusCode = 500;
+        throw error;
+
     }
 }
 
-module.exports = handler;
+module.exports = origin ? microCors({ origin })(handler) : handler;
