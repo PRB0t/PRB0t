c8d994445db17107b5f6024cea7f54417a6ff7ca
j-rewerts <jaredrewerts@gmail.com>
2019-05-14 13:08:37 -0600

    Updated author to PRB0t.

--- src/handlers/micro.js
+++ src/handlers/micro.js
@@ -6,19 +6,21 @@ const microCors = require('micro-cors');
 const handler = async (req, res) => {
 
     try {
+
         const body = await json(req);
-
-        // Set the commit author if requested.
-        let author = {
-            name: 'canada-bot',
-            email: 'canada.pr.bot@gmail.com',
-        };
-        if (body.author) {
-            author = body.author
-        }
-
         const pr = new PR(body.user, body.repo, body.branch, body.token);
-        pr.configure(body.files, body.commit, body.title, body.description, author);
+
+        pr.configure(
+            body.files,
+            body.commit,
+            body.title,
+            body.description,
+            body.author || {
+                name: 'PRB0t',
+                email: '34620110+PRB0t@users.noreply.github.com',
+            }
+        );
+
         const { data } = await pr.send();
         return data;
 
